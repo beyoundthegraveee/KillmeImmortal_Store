bac8125fd6f91d5b653d63170c1e655393aa6c73
beyoundthegraveee <jackertfun228@mail.ru>
2024-12-02 20:39:44 +0100

    fixed searc

--- frontend/static/js/search.js
+++ frontend/static/js/search.js
@@ -1,8 +1,23 @@
-document.getElementById('search-input').addEventListener('input', function() {
-    const query = this.value.trim();
-    if (query) {
+
+const search_input = document.getElementById('search-input');
+const search_button = document.querySelector('.search-icon');
+
+
+search_input.addEventListener('keyup', function(event) { 
+    if (event.key === 'Enter') { 
+        performSearch(this.value.trim()); 
+    } 
+}); 
+search_button.addEventListener('click', function() { 
+    const query = search_input.value.trim(); 
+    performSearch(query); 
+});
+
+async function performSearch(query) { 
+    if (query) { 
         window.location.href = `/search?q=${query}`;
-    } else {
+    } else { 
         window.location.href = '/search';
     }
-});
+}
+
